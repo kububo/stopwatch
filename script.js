c877c99dff858d7b97ad157bb97a50f536c32842
Martin Oliva <martinekoliva@gmail.com>
2021-10-13 12:41:44 +0200

    Upgrade 🔨

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 let time = 0;
+let interval;
 
 const currentTime = document.querySelector('.time');
-const button = document.querySelector('.start');
+const timesList = document.querySelector('.times');
+const startButton = document.querySelector('.start');
+const holdButton = document.querySelector('.hold');
+const stopButton = document.querySelector('.stop');
 
-button.addEventListener('click', () => {
-	setInterval(() => {
+startButton.addEventListener('click', () => {
+	startButton.setAttribute('disabled', true);
+	stopButton.removeAttribute('disabled');
+
+	time = 0;
+
+	interval = setInterval(() => {
 		time += 0.1;
 
 		currentTime.innerText = time.toFixed(1);
 	}, 100);
+
+	timesList.innerHTML = '';
+});
+
+stopButton.addEventListener('click', () => {
+	startButton.removeAttribute('disabled');
+	stopButton.setAttribute('disabled', true);
+
+	clearInterval(interval);
+});
+
+holdButton.addEventListener('click', () => {
+	const item = document.createElement('li');
+	item.innerText = time.toFixed(1);
+	timesList.prepend(item);
 });
